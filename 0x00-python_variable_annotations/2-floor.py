8c4cf08edf8c30781dbe9bbbec86b636ecaeaf08
Mwmuraguri <maryannewanjiku87@gmail.com>
2024-01-11 15:18:36 +0000

    variable annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+a function to return the floor of a number
+@n: the argument to pass
+return an int floor
+"""
+
+
+def floor(n: float) -> int:
+    """ a function that returns the floor of a number"""
+    return int(n // 1)
